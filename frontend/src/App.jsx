7aec722e492c52d9673525f1e7b049dbc763b896
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-19 11:58:30 +0300

    Update Contact, Blog and App,jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,24 @@ import Home from './components/home/Home'
 import About from './components/about/About'
 import Resume from './components/resume/Resume'
 import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Blog from './components/blog/Blog'
 
 const App = () => {
   return (
     <div>
-      
+      <Sideber />
+      <main className=''>
+        <Home />
+        <About />
+        <Resume />
+        <Services />
+        <Testimonials />
+        <Portfolio />
+        <Blog />
+        <Contact />
+      </main>
     </div>
   )
 }
