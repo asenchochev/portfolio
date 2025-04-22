bdb149cdf27f82d7fcf6a2bf6a7e1f9beaa2d651
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-22 14:46:56 +0300

    Import NavBarLinks, button and Logo to Main

---
+++ frontend/src/components/navbar/NavBarMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBarLogo from './NavBarLogo'
+import NavBarLinks from './NavBarLinks'
+import NavBarBtn from './NavBarBtn'
+
+const NavBarMain = () => {
+  return (
+    <div>
+      <NavBarLogo />
+      <NavBarLinks />
+      <NavBarBtn />
+    </div>
+  )
+}
+
+export default NavBarMain
