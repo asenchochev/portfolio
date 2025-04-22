6a160f3aadf306976515ae9190a375999bd54a1c
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-22 18:28:06 +0300

    Create SubHero

--- frontend/src/components/contactMeSection/ContactInfo.jsx
+++ frontend/src/components/contactMeSection/ContactInfo.jsx
@@ -6,9 +6,9 @@ import SingleInfo from "./SingleInfo";
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-4">
-      <SingleInfo text="abdelrahman.qassem@gmail.com" Image={HiOutlineMail} />
-      <SingleInfo text="+2 01000 66 24 55" Image={FiPhone} />
-      <SingleInfo text="Alexandria, Egypt" Image={IoLocationOutline} />
+      <SingleInfo text="asenchochochev@yahoo.com" Image={HiOutlineMail} />
+      <SingleInfo text="+359893934322" Image={FiPhone} />
+      <SingleInfo text="Kazanlak, Bulgaria" Image={IoLocationOutline} />
     </div>
   );
 };
