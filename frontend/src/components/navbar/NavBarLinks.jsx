9adaf459896938ad30ad4e82bfef67ce3f6f333f
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-22 14:47:13 +0300

    Update NavBarLinks, Logo, Button

---
+++ frontend/src/components/navbar/NavBarLinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBarLinks = () => {
+  return (
+    <div>
+      <h1>Links</h1>
+    </div>
+  )
+}
+
+export default NavBarLinks
---
+++ frontend/src/components/navbar/NavBarLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBarLogo = () => {
+  return (
+    <div>
+      <h1>Asen Chochev</h1>
+    </div>
+  )
+}
+
+export default NavBarLogo
