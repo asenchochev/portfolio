be6cd75801aa412a576e62344be9f94607ebc339
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-22 14:54:54 +0300

    Update initialize

--- frontend/src/components/navbar/NavBarMain.jsx
+++ frontend/src/components/navbar/NavBarMain.jsx
@@ -5,11 +5,13 @@ import NavBarBtn from './NavBarBtn'
 
 const NavBarMain = () => {
   return (
+    <nav className='max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2    '>
     <div>
       <NavBarLogo />
       <NavBarLinks />
       <NavBarBtn />
     </div>
+    </nav>
   )
 }
 
