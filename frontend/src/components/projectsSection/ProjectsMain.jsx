9dbe0f2372409f35c7a2cadbf0b135a77ccda3e0
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-22 18:13:18 +0300

    Create Project section with uploaded images for my projects

---
+++ frontend/src/components/projectsSection/ProjectsMain.jsx
@@ -0,0 +1,51 @@
+import ProjectsText from "./ProjectsText";
+import SingleProject from "./SingleProject";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
+
+const projects = [
+  {
+    name: "Weather App",
+    year: "Apr2025",
+    align: "right",
+    image: "../../public/images/weather.png",
+    link: "https://weatherapp-two-murex.vercel.app/",
+  },
+  {
+    name: "Estate App",
+    year: "April2025",
+    align: "left",
+    image: "../../public/images/estate.png",
+    link: "https://github.com/asenchochev/estate-app",
+  },
+];
+
+const ProjectsMain = () => {
+  return (
+    <div id="projects" className="max-w-[1200px] mx-auto px-4">
+      <motion.div
+        variants={fadeIn("top", 0)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.7 }}
+      >
+        <ProjectsText />
+      </motion.div>
+      <div className="flex flex-col gap-20 max-w-[900px] mx-auto mt-12">
+        {projects.map((project, index) => {
+          return (
+            <SingleProject
+              key={index}
+              name={project.name}
+              year={project.year}
+              align={project.align}
+              image={project.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsMain;
